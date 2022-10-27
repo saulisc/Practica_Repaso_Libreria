db9d82254ef7158a6246396c71a7bdca57c84b39
Saul Bonilla <jobskevs@hotmail.com>
2022-10-27 12:12:45 -0500

    add method post to procces form

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControladorVistas;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('home',[ControladorVistas::class,'showHome']) -> name('home');
+Route::get('registro',[ControladorVistas::class,'showRegistro']) -> name('registro');
 
-//Route::view('home','home') -> name('home');
-Route::view('registro','registro') -> name('registro');
+Route::post('procesarFormulario',[ControladorVistas::class,'procesarFormulario']) -> name('procesarFormulario');
